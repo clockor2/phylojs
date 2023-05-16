4931cd66c8b9ebe4671e68d900fe6f2dbc81e26d
Wytamma Wirth <wytamma.wirth@me.com>
2023-05-16 10:32:08 +1000

    style: format tests

--- test/Reader.spec.ts
+++ test/Reader.spec.ts
@@ -5,7 +5,7 @@ describe('TreeFromNewick', () => {
   test('init', () => {
     const inNewick = '("A":1,("B":1,"C":1):1):0.0;';
     const tree = readNewick(inNewick);
-    const outNewick = writeNewick(tree); 
+    const outNewick = writeNewick(tree);
     expect(outNewick).toBe(inNewick);
   });
 });
