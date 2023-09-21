bd2b0f16c9281dcd83944c39e3634f0d00dc88a6
Wytamma Wirth <wytamma.wirth@me.com>
2023-09-21 16:13:20 +1000

    fix: reoder import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { Node } from './node';
-export { Tree } from './tree';
 export { writeNewick } from './io/writers/newick';
 export { writeNexus } from './io/writers/nexus';
 export { readNewick, readTreesFromNewick } from './io/readers/newick';
@@ -7,3 +5,5 @@ export { readNexus, readTreesFromNexus } from './io/readers/nexus';
 export { readPhyloXML, readTreesFromPhyloXML } from './io/readers/phyloXML';
 export { readNeXML, readTreesFromNeXML } from './io/readers/neXML';
 export { read } from './io/readers/tree';
+export { Node } from './node';
+export { Tree } from './tree';
