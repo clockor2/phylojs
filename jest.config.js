c30b02a6bb4887c82ec3ba9d4f402e0f308a17d6
LeoFeatherstone <leofeatherstone@protonmail.com>
2025-05-04 11:33:50 +1000

    fix(acti): increase node to v22 (lts) to attempt avoiding memory leak in   release actions

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,24 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const paths = {
-  "@phylojs": ["src"],
-  "@phylojs/*": ["src/*"],
+    "@phylojs": ["src"],
+    "@phylojs/*": ["src/*"],
 }
 
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['**/test/**/*.spec.ts'],
-  moduleNameMapper: pathsToModuleNameMapper(paths, {
-    prefix: '<rootDir>/',
-  }),
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/types/**/*.ts',
-  ],
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-      isolatedModules: true,
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    testMatch: ['**/test/**/*.spec.ts'],
+    moduleNameMapper: pathsToModuleNameMapper(paths, {
+        prefix: '<rootDir>/',
+    }),
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/types/**/*.ts',
+    ],
+    globals: {
+        'ts-jest': {
+            diagnostics: false,
+            isolatedModules: true,
+        },
     },
-  },
 };
